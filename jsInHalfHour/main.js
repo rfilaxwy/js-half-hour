d30d3f38be58c2cbb389b64d19f453f38212fc6b
Ryan Filax-Wylie <ryanfilaxwylie@gmail.com>
2017-11-02 14:56:58 -0600

    Completed tutroial

--- jsInHalfHour/main.js
+++ jsInHalfHour/main.js
@@ -2,16 +2,25 @@ var tits = document.getElementById("inTitle");
 var listItems = document.getElementById("ourlist").getElementsByTagName('li');
 var ourButton = document.getElementById('our-button');
 var newCounter = 1;
-for (var i =0; i< listItems.length; i++){
-	listItems[i].addEventListener("click", activateItem);
+
+ourlist.addEventListener('click',activateItem);
+
+function activateItem(e){
+	if(e.target.nodeName == "LI"){
+		tits.innerHTML = e.target.innerHTML;
+	for (var i =0; i< listItems.length; i++){
+	listItems[i].classList.remove("active");
+	}
+	e.target.classList.add('active');
+	
+	}
 }
 
-function activateItem(){
-	tits.innerHTML = this.innerHTML;
-}
 ourButton.addEventListener("click", newItem);
+
 function newItem(){
 	ourlist.innerHTML+="<li>new stuff "+newCounter+"</li>"; 
 	newCounter+=1;
-
 }
+
+
